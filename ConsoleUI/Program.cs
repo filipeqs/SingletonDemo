d99f83a734d83e1c41eccbcb9eecd9c23cfa62bf
Filipe Silva <filipeqs@outlook.com>
2021-08-15 20:26:08 -0400

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+
+        static void Main(string[] args)
+        {
+            var servers = TableServers.GetTableServers();
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine("The next server is: " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host1List.GetNextServer());
+        }
+
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server is: " + host2List.GetNextServer());
+        }
+    }
+}
---
+++ ConsoleUI/TableServers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instace = new TableServers();
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+
+        private TableServers()
+        {
+            servers.Add("Tim");
+            servers.Add("Sue");
+            servers.Add("Mary");
+            servers.Add("Bob");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instace;
+        }
+
+        public string GetNextServer()
+        {
+            var output = servers[nextServer];
+            nextServer += 1;
+            if (nextServer >= servers.Count)
+                nextServer = 0;
+
+            return output;
+        }
+    }
+}
